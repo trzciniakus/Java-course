90e24c25aa148c2ea4b6392a38dd749f7e7151fc
trzciniakus <artur.trzcinski.pn@gmail.com>
2020-10-20 15:31:11 +0200

    create Car.java, Main.java in CH5_ClassesTraining

---
+++ src/CH5_ClassesTraining/Car.java
@@ -0,0 +1,12 @@
+package CH5_ClassesTraining;
+
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+
+
+}
---
+++ src/CH5_ClassesTraining/Main.java
@@ -0,0 +1,13 @@
+package CH5_ClassesTraining;
+
+import CH5_ClassesTraining.Car;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car porsche = new Car();
+        Car holden = new Car();
+
+
+    }
+}
