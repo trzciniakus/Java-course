871c939856034dc26c884a6f8f31638afe18b37a
trzciniakus <artur.trzcinski.pn@gmail.com>
2020-11-09 20:25:01 +0100

    create CH9.InnerClassChallenge

---
+++ src/CH9/InnerClassChallenge/Album.java
@@ -0,0 +1,70 @@
+package CH9.InnerClassChallenge;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Album {
+    private String name;
+    private String artist;
+    private SongList songs;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new SongList();
+    }
+
+    public boolean addSong(String title, double duration) {
+        return songs.addSong(title, duration);
+    }
+
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> playList) {
+        int index = trackNumber - 1;
+        if ((index > 0) && (index <= this.songs.getSongList().size())) {
+            playList.add(this.songs.getSongList().get(index));
+            return true;
+        }
+        System.out.println("This album does not have a track " + trackNumber);
+        return false;
+    }
+
+    public boolean addToPlayList(String title, LinkedList<Song> playList) {
+        Song checkedSong = songs.findSong(title);
+        if (checkedSong != null) {
+            playList.add(checkedSong);
+            return true;
+        }
+        System.out.println("The song " + title + " is not in this album");
+        return false;
+    }
+
+    private class SongList {
+        private ArrayList<Song> songList;
+
+        public SongList() {
+            this.songList = new ArrayList<Song>();
+        }
+
+        private boolean addSong(String title, double duration) {
+            if (findSong(title) == null) {
+                this.songList.add(new Song(title, duration));
+                return true;
+            }
+            return false;
+        }
+
+        private Song findSong(String title) {
+            for (Song checkedSong : this.songList) {
+                if (checkedSong.getTitle().equals(title)) {
+                    return checkedSong;
+                }
+            }
+            return null;
+        }
+
+        public ArrayList<Song> getSongList() {
+            return songList;
+        }
+    }
+}
+
