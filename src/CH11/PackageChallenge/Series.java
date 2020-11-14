6982a3fd4942735ff63f3fa1142f6c15451749a8
trzciniakus <artur.trzcinski.pn@gmail.com>
2020-11-14 22:56:02 +0100

    create CH11.PackageChallenge

---
+++ src/CH11/PackageChallenge/Series.java
@@ -0,0 +1,41 @@
+package CH11.PackageChallenge;
+
+public class Series {
+
+    public static int nSum(int n) {
+        if (n < 0) {
+            return -1;
+        }
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static int factorial(int n) {
+        if (n < 0) {
+            return -1;
+        } else if (n==0){
+            return 0;
+        }
+        int product = 1;
+        for (int i = 1; i <= n; i++) {
+            product *= i;
+        }
+        return product;
+    }
+
+    public static int fibonacci(int n) {
+        if (n < 0) {
+            return -1;
+        } else if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
+}
