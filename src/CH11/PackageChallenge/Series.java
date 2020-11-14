5681acfa983f736ad90b32d7f27c4c7674fbda69
trzciniakus <artur.trzcinski.pn@gmail.com>
2020-11-14 22:59:39 +0100

    change CH11.PackageChallenge

--- src/CH11/PackageChallenge/Series.java
+++ src/CH11/PackageChallenge/Series.java
@@ -16,8 +16,6 @@ public class Series {
     public static int factorial(int n) {
         if (n < 0) {
             return -1;
-        } else if (n==0){
-            return 0;
         }
         int product = 1;
         for (int i = 1; i <= n; i++) {
